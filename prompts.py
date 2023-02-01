ff92315e587cc77d7c6b0537b44c03b1f4d54b56
Rahul Vaghasia <rjvaghasia@gmail.com>
2023-02-01 15:31:52 -0500

    Secondary commit

--- prompts.py
+++ prompts.py
@@ -3,6 +3,6 @@ import requests
 #r = requests.post("http://127.0.0.1:5000/getresponse",data={'Character': input("Who do you want to interview: ")})
 
 
-for i in range(20):
+for i in range(30):
     r = requests.post("http://127.0.0.1:5000/getresponse", data={'query': input("Question: ")})
     print(r.text, "\n")
